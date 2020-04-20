7e5295d8ac1eca4b90d357eaf86ca977a5d73aaf
Gigo <gigo@hk.edu.tw>
2020-04-20 10:44:16 +0800

    完成專案

--- app/src/main/java/com/gigoallen/imgcheckboxapp/MainActivity.java
+++ app/src/main/java/com/gigoallen/imgcheckboxapp/MainActivity.java
@@ -2,6 +2,9 @@ package com.gigoallen.imgcheckboxapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btnOK(View view) {
+
+
+
+
+        CheckBox chk;
+
+        ImageView imgMina, imgMomo, imgSana, imgTzuyu;
+
+
+        imgMina = findViewById(R.id.imgMina);
+        imgMomo = findViewById(R.id.imgMomo);
+        imgSana = findViewById(R.id.imgSana);
+        imgTzuyu = findViewById(R.id.imgTzuyu);
+
+        //先將圖片隱藏
+        imgMina.setVisibility(View.GONE);
+        imgMomo.setVisibility(View.GONE);
+        imgSana.setVisibility(View.GONE);
+        imgTzuyu.setVisibility(View.GONE);
+
+        //array
+        int[] id = {R.id.chkMina, R.id.chkMomo, R.id.chkSana, R.id.chkTzuyu};
+
+        for(int i:id){
+
+            chk = findViewById(i);
+
+            if(chk.isChecked()){
+
+                if(chk.getId() == R.id.chkMina)
+                    imgMina.setVisibility(View.VISIBLE);
+                if(chk.getId() == R.id.chkMomo)
+                    imgMomo.setVisibility(View.VISIBLE);
+                if(chk.getId() == R.id.chkSana)
+                    imgSana.setVisibility(View.VISIBLE);
+                if(chk.getId() == R.id.chkTzuyu)
+                    imgTzuyu.setVisibility(View.VISIBLE);
+
+            }
+
+        }
     }
 }
